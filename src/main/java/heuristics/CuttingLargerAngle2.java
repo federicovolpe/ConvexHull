d953d585107e7a02d894720d64a97c26c1246ca2
Federico <91740549+federicovolpe@users.noreply.github.com>
2024-07-26 16:18:32 +0200

    cuttingLargerAngle2 readapted and tested

--- src/main/java/heuristics/CuttingLargerAngle2.java
+++ src/main/java/heuristics/CuttingLargerAngle2.java
@@ -4,14 +4,14 @@ import basic.Edge;
 import basic.Node2D;
 import java.util.List;
 
-public class CuttingNodes2 extends CuttingNodes{
+public class CuttingLargerAngle2 extends CuttingLargerAngle {
 
   /**
    * constructor which instanciates all the computation
    * @param n number of desired edges
    * @param convexHull official convex hull
    */
-  public CuttingNodes2 (int n, final List<Edge> convexHull) {
+  public CuttingLargerAngle2(int n, final List<Edge> convexHull) {
     super(n, convexHull);
   }
 
@@ -21,31 +21,35 @@ public class CuttingNodes2 extends CuttingNodes{
   protected void applyCut(){
     int selected = selectAngle();
     Edge lowestA  = convexHull.get(selected);
+    Node2D cuttedNode = lowestA.n2();
     Edge lowestB = convexHull.get(selected+1);
 
     //cutting the node
+    System.out.println("selected node: " + convexHull.get(selected).n2());
     convexHull.set(selected, new Edge(lowestA.n1(), lowestB.n2()));
+    Edge selectedEdge = convexHull.get(selected);
+    System.out.println("new edge : "+ selectedEdge);
+
     convexHull.remove(lowestB);
+    int selectedEdgeIndex = convexHull.indexOf(selectedEdge);
 
     // traslating the new edge
-    Node2D centerOfMass = lowestA.getCenterOfMass(lowestB);
-     System.out.println("traslating "+ convexHull.get(selected));
-    convexHull.get(selected).traslate(centerOfMass);
+    System.out.println("traslating "+ selectedEdge);
+    selectedEdge.traslate(cuttedNode);
 
     // extending the edges according tho the center of mass
-    extendEdges(convexHull.get(selected -1), convexHull.get(selected), convexHull.get(selected+1));
+    System.out.println("extending edges: "+ convexHull.get(selectedEdgeIndex-1) +" "+convexHull.get(selectedEdgeIndex+1));
+    extendEdges(convexHull.get(selectedEdgeIndex-1), convexHull.get(selectedEdgeIndex), convexHull.get(selectedEdgeIndex+1));
   }
 
 
   protected void extendEdges (Edge prev, Edge toExtend, Edge succ){
-    // System.out.println("extending "+ prev);
     Node2D intersectionPrev = toExtend.calcIntersectionWithLine(prev.getLineParameters()[0],
         prev.getLineParameters()[1],
         prev.getLineParameters()[2]);
     prev.setn2(intersectionPrev);
     toExtend.setn1(intersectionPrev);
 
-    // System.out.println("extending "+ succ);
     Node2D intersectionSucc = toExtend.calcIntersectionWithLine(succ.getLineParameters()[0],
         succ.getLineParameters()[1],
         succ.getLineParameters()[2]);
